7a56b2bddc268c76f810d6d383ca8d48c3388c24
Charles Agoe <agoetee@gmail.com>
2024-07-25 17:14:36 +0000

    add Order_details_id table

--- task_2.sql
+++ task_2.sql
@@ -19,9 +19,18 @@ CREATE TABLE Customers (
     address TEXT
 )
 
-CREATE TABLE Order_Details (
+CREATE TABLE Orders (
     order_id INT AUTO INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+
+CREATE TABLE Order_Details (
+    order_details_id INT AUTO INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
