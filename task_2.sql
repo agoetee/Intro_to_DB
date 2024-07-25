b683a987e806c06d35e56d2ee7e2d8c665d66851
Charles Agoe <agoetee@gmail.com>
2024-07-25 17:09:30 +0000

    create magical tables

---
+++ task_2.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Books (
+    book_id INT AUTO INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+)
+
+CREATE TABLE Authors (
+    author_id INT AUTO INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+)
+
+CREATE TABLE Customers (
+    customer_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    customer_email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE Orders (
+    order_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
